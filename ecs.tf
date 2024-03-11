9d95efe41a3b4fbb1e6cfce8cedd31fd2183bd0f
Durga4pdp <92569197+Durga4pdp@users.noreply.github.com>
2024-03-11 12:57:06 +0530

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -346,7 +346,7 @@ resource "aws_ecs_service" "app" {
   load_balancer {
     target_group_arn = aws_lb_target_group.app.arn
     container_name   = "app"
-    container_port   = 80
+    container_port   = 3000
   }
 }
 
